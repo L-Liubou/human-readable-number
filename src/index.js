bc4cabf9bf3f63c59e6d925e2ac1ecc36ef8b51d
Liubou Levitskaya <lyubasaraeva@gmail.com>
2024-08-29 17:16:28 +0300

    feat: realize toReadable function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    
+    const prime = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    const decimals = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const hundredths = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    if (number < 10) {
+        return prime[number];
+    } else if (number < 20) {
+        return decimals[number - 10]; 
+    } else if (number < 100) {
+        return hundredths[Math.floor(number / 10)] + (number % 10 !== 0 ? ' ' + prime[number % 10] : '');
+    } else {
+        return prime[Math.floor(number / 100)] + ' hundred' +
+            (number % 100 !== 0 ? ' ' + toReadable(number % 100) : '');
+    }
 }
+
+
